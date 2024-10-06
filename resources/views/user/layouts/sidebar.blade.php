0d372d6a8980f8fd6505cf81613d95d81603dfd2
QuocAnh189 <anhquoc18092003@gmail.com>
2024-10-06 23:44:28 +0700

    update sidebar user

--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -16,29 +16,29 @@
                 </a>
             </li>
 
-            <li class="{{ setActive(['']) }}">
-                <a class="nav-link" href="">
+            <li class="{{ setActive(['user.projects.*']) }}">
+                <a class="nav-link" href="{{ route('user.projects.index') }}">
                     <i class="fas fa-book"></i>
                     <span>Projects</span>
                 </a>
             </li>
 
-            <li class="{{ setActive(['']) }}">
-                <a class="nav-link" href="">
+            <li class="{{ setActive(['user.project-galleries.*']) }}">
+                <a class="nav-link" href="{{ route('user.project-galleries.index') }}">
                     <i class="fas fa-image"></i>
-                    <span>Project Galleries</span>
+                    <span>Galleries</span>
                 </a>
             </li>
 
-            <li class="{{ setActive(['']) }}">
-                <a class="nav-link" href="">
+            <li class="{{ setActive(['user.features.*']) }}">
+                <a class="nav-link" href="{{ route('user.features.index') }}">
                     <i class="fas fa-x-ray"></i>
                     <span>Feature</span>
                 </a>
             </li>
 
-            <li class="{{ setActive(['']) }}">
-                <a class="nav-link" href="">
+            <li class="{{ setActive(['user.links.*']) }}">
+                <a class="nav-link" href="{{ route('user.links.index') }}">
                     <i class="fas fa-link"></i>
                     <span>Links</span>
                 </a>
